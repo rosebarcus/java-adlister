5250065c5cb1ff89bd1596292e761914f557469f
Rose Barcus <angela.rose.barcus@gmail.com>
2021-06-29 13:51:14 -0500

    following along to the lecture

---
+++ src/main/java/AdminServlet.java
@@ -0,0 +1,29 @@
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "AdminServlet", urlPatterns = "/admin")
+public class AdminServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        try {
+            boolean isAdmin = req.getParameter("isAdmin").equalsIgnoreCase("yes");
+
+            if (isAdmin) {
+                out.println("<h1>Welcome home, Admin.</h1>");
+            } else {
+                resp.sendRedirect("/hello-world");
+            }
+        } catch (NullPointerException npe) {
+            System.out.println(npe);
+            resp.sendRedirect("/hello-world");
+        }
+    }
+}
