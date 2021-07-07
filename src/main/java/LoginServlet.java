ba4d78208a6d637def1349ec850b567957d9e74e
Rose Barcus <angela.rose.barcus@gmail.com>
2021-07-07 11:21:13 -0500

    following lecture notes

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,23 +3,50 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       if(request.getSession().getAttribute("isAdmin") != null) {
+           response.sendRedirect("/secret-admin-page");
+           return;
+       }
+
         request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        boolean validAttempt = username.equals("admin") && password.equals("password");
+        boolean isAdmin = username.equals("admin") && password.equals("password");
 
-        if (validAttempt) {
-            response.sendRedirect("/profile");
+        HttpSession session = request.getSession();
+
+        if (isAdmin) {
+            session.setAttribute("isAdmin", true);
+            response.sendRedirect("/secret-admin-page");
         } else {
             response.sendRedirect("/login");
         }
+        /*// set the session object to a variable based on the HttpSession class.
+        HttpSession session = request.getSession();
+
+        String darkMode = request.getParameter("color");
+        // set the dark theme attribute on the session so that it gets remembered every time we log in.
+        session.setAttribute("darkMode", darkMode);
+
+        // on a different servlet (profile page)
+        // with our getAttribute method, our return is always a generic object type.
+        // therefore we need to explicitly cast the object to the correct type.
+        session.getAttribute("darkMode");
+        String darkSelected = (String) session.getAttribute("darkMode");
+
+        // to remove attribute
+        session.removeAttribute("darkMode");
+
+        // destroys the session
+        session.invalidate();*/
     }
 }
---
+++ src/main/webapp/secret-admin-page.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: rosebarcus
+  Date: 7/7/21
+  Time: 10:58 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Secret Admin Page</title>
+</head>
+<body>
+    <h1>Shhh, it's a secret!</h1>
+</body>
+</html>
